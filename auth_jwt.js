271addd97c5eb71c7b29695abcafe14a55e80663
yvesnsenga <46980788+yvesnsenga@users.noreply.github.com>
2019-02-15 00:27:23 -0700

    Update code

--- auth_jwt.js
+++ auth_jwt.js
@@ -5,7 +5,7 @@ var ExtractJwt = require('passport-jwt').ExtractJwt;
 
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-opts.secretOrKey = process.env.SECRET_KEY;
+opts.secretOrKey = process.env.UNIQUE_KEY;
 
 passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     var user = db.find(jwt_payload.id);
--- server.js
+++ server.js
@@ -31,7 +31,7 @@ function getJSONObject(req) {
     return json;
 }
 
-router.route('/post')
+router.route('/postjwt')
     .post(authController.isAuthenticated, function (req, res) {
             console.log(req.body);
             res = res.status(200);
@@ -81,7 +81,7 @@ router.post('/signin', function(req, res) {
         // check if password matches
         if (req.body.password == user.password)  {
             var userToken = { id : user.id, username: user.username };
-            var token = jwt.sign(userToken, process.env.SECRET_KEY);
+            var token = jwt.sign(userToken, process.env.UNIQUE_KEY);
             res.json({success: true, token: 'JWT ' + token});
         }
         else {
