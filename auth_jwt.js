4b85703b185add6e7363d537b47a11b424502cc0
yvesnsenga <46980788+yvesnsenga@users.noreply.github.com>
2019-02-22 23:57:55 -0700

    secret key

--- auth_jwt.js
+++ auth_jwt.js
@@ -2,11 +2,9 @@
 var passport = require('passport');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
-
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
 opts.secretOrKey = process.env.UNIQUE_KEY;
-
 passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     var user = db.find(jwt_payload.id);
 
